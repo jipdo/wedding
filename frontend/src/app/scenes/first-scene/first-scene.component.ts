ec30ea5fd2236d5c89f088325be53644233f56cc
yuyupopo <yuyupopo@snu.ac.kr>
2021-01-29 14:14:14 +0900

    Add example first scene

--- frontend/src/app/scenes/first-scene/first-scene.component.ts
+++ frontend/src/app/scenes/first-scene/first-scene.component.ts
@@ -7,31 +7,56 @@ import { ProgressFrameService } from '../../progress-frame.service';
   templateUrl: './first-scene.component.html',
   styleUrls: ['./first-scene.component.scss']
 })
-export class FirstSceneComponent implements OnInit {
+export class FirstSceneComponent implements OnInit, AfterViewInit {
 
   @ViewChild('canvas')
   canvas!: ElementRef<HTMLCanvasElement>;
 
-  context!: CanvasRenderingContext2D;
-  numFrame = 100;
+  @ViewChild('hand')
+  handImage!: ElementRef<HTMLImageElement>;
+
+  ctx!: CanvasRenderingContext2D;
+  numFrame = 1000;
 
   constructor(private progressFrameService: ProgressFrameService) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   ngAfterViewInit(): void {
-    this.context = this.canvas.nativeElement.getContext('2d')!;
+    this.ctx = this.canvas.nativeElement.getContext('2d')!;
+    this.ctx.canvas.width  = window.innerWidth;
+    this.ctx.canvas.height = window.innerHeight;
 
     this.progressFrameService.watchProgress(this.numFrame)
       .subscribe(f => this.draw(f));
   }
 
   draw(f: number) {
-    this.context.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
-    this.context.beginPath();
-    this.context.fillRect(this.canvas.nativeElement.width * f / this.numFrame, 50, 50, 50);
-    this.context.closePath();
+    const width = this.canvas.nativeElement.width;
+    const height = this.canvas.nativeElement.height;
+    this.ctx.clearRect(0, 0, width, height);
+
+    this.ctx.beginPath();
+    const offsetTop = 96;
+    const offsetBottom = 75;
+    const objHeight = 40;
+    const pos = {
+      x: 174,
+      y: offsetTop + (height - offsetTop - offsetBottom - objHeight) * f / this.numFrame,
+      width: 305,
+      height: objHeight,
+    };
+    this.ctx.fillStyle = 'grey';
+    this.ctx.fillRect(pos.x, pos.y, pos.width, pos.height);
+    this.ctx.closePath();
+
+    this.ctx.fillStyle = 'black';
+    this.ctx.font = '30px Arial';
+    this.ctx.fillText('Wedding!', pos.x + pos.width / 3, pos.y + pos.height * 2 / 3);
+
+
+    this.ctx.drawImage(this.handImage.nativeElement, 0, 0);
+    // draw hand
   }
 
 }
